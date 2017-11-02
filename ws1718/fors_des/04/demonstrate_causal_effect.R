81db5d4af35b92948d133247b1b9dcf7cc210d73
Dag Tanneberg <dag.tanneberg@googlemail.com>
2017-11-02 05:49:29 +0100

    Summary 10-30-2017

---
+++ ws1718/fors_des/04/demonstrate_causal_effect.R
@@ -0,0 +1,59 @@
+# This script generates a graphical explanation of KKV's notion
+# of causality. It returns a pdf.
+# setup workspace ==========================================
+rm(list = ls())
+set.seed(12345)
+library(extrafont)
+loadfonts()
+
+# definitions ----------------------------------------------
+N <- 30
+x <- runif(N, min = 0, max = 1)
+realized_x <- quantile(x, .1)
+
+# generate plot --------------------------------------------
+pdf(
+  file = "~/github/teaching/ws1718/fors_des/04/demonstrate_causal_effect.pdf",
+  width = 7, height = 1.2, family = "CMU Sans Serif"
+)
+par(bg = '#F0F0F0', mfrow = c(1, 3), mai = c(0, 0, .5, .1))
+
+# (1) realized causal effect
+plot(
+  x, dunif(x), xlim = c(0, 1), ylim = c(.9, 1), typ = 'n',
+  xaxt = "n", yaxt = 'n', xlab = '', ylab = '', bty = 'n',
+  main = "Realized causal effect"
+)
+points(realized_x, dunif(realized_x), pch = 16, col = 'black')
+text(
+  x = realized_x, y = dunif(realized_x),
+  label = expression(y[i]^T - y[i]^C), adj = c(.5, 1.4)
+)
+
+# (2) random causal effect
+plot(
+  x, dunif(x), xlim = c(0, 1), ylim = c(.9, 1), typ = 'n',
+  xaxt = "n", yaxt = 'n', xlab = '', ylab = '', bty = 'n',
+  main = "Random causal effect"
+)
+
+points(x, dunif(x), pch = 16, col = 'grey65', cex = .8)
+points(realized_x, dunif(realized_x), pch = 16, col = 'black')
+text(
+  x = 0.5, y = dunif(0.5),
+  label = expression(Y[i]^T - Y[i]^"C"), adj = c(0.5, 1.4)
+)
+
+# (3) mean causal effect
+plot(
+  x, dunif(x), xlim = c(0, 1), ylim = c(.9, 1), typ = 'n',
+  xaxt = "n", yaxt = 'n', xlab = '', ylab = '', bty = 'n',
+  main = "Mean causal effect"
+)
+points(x, dunif(x), pch = 16, col = 'grey65', cex = .8)
+points(realized_x, dunif(realized_x), pch = 16, col = 'black')
+points(.5, dunif(.5), pch = 16, col = "red")
+text(x = 0.5, y = dunif(0.5),
+  label = expression(mu[i]^T - mu[i]^"C"), adj = c(0.5, 1.4)
+)
+dev.off()
--- ws1718/fors_des/04/mean_causal_effect.R
+++ ws1718/fors_des/04/mean_causal_effect.R
@@ -5,18 +5,42 @@ normal_density <- function(x, mu = 0, sigma = 1){
   (1 / (sigma * sqrt(2 * pi))) * exp(-1 * (x - mu)^2 / (2 * sigma^2))
 }
 
-mu <- 0
-sigma <- 1
-
-N <- 1
+N <- 30
 x <- rnorm(N)
-plot(
-  x, dnorm(x), xlim = c(-3, 3), ylim = c(0, .5),
-  xaxt = "n", bty = 'n'
-)
-curve(normal_density, from = -3, to = 3, add = TRUE)
-points(0, dnorm(0), pch = 21, bg = "red")
-segments(x0 = 0, y0 = 0, x1 = 0, y1 = dnorm(0), lty = "dashed")
-axis(side = 1, at = 0, labels = expression(mu[i]^T - mu[i]^"C"))
-text(x = x[1], y = dnorm(x[1]), label = expression("Realized causal effect":~y[i]^T - y[i]^C), adj = c(-.1, 0))
 
+pdf(file = "test.pdf", width = 7, height = 7/1.618)
+par(bg = '#F0F0F0', mfrow = c(1, 3))
+plot(
+  x, dnorm(x), xlim = c(-3, 3), ylim = c(0, .5), typ = 'n',
+  xaxt = "n", yaxt = 'n', xlab = '', ylab = '', bty = 'n', bg = 'black',
+  main = "Realized causal effect:"
+)
+points(min(x), dnorm(min(x)), pch = 16, col = 'black', cex = 1.5)
+text(x = min(x), y = dnorm(min(x)), label = expression(y[i]^T - y[i]^C), adj = c(1.05, 0))
+
+plot(
+  x, dnorm(x), xlim = c(-3, 3), ylim = c(0, .5), typ = 'n',
+  xaxt = "n", yaxt = 'n', xlab = '', ylab = '', bty = 'n', bg = 'black',
+  main = "Random causal effect:"
+)
+curve(normal_density, from = -3, to = 3, add = TRUE, col = 'grey65')
+points(x, dnorm(x), pch = 16, col = 'grey65')
+points(min(x), dnorm(min(x)), pch = 16, col = 'black', cex = 1.5)
+text(x = min(x), y = dnorm(min(x)), label = expression(y[i]^T - y[i]^C), adj = c(1.05, 0))
+
+plot(
+  x, dnorm(x), xlim = c(-3, 3), ylim = c(0, .5), typ = 'n',
+  xaxt = "n", yaxt = 'n', xlab = '', ylab = '', bty = 'n', bg = 'black',
+  main = "Random causal effect:"
+)
+curve(normal_density, from = -3, to = 3, add = TRUE, col = 'grey65')
+points(x, dnorm(x), pch = 16, col = 'grey65')
+points(min(x), dnorm(min(x)), pch = 16, col = 'black', cex = 1.5)
+text(x = min(x), y = dnorm(min(x)), label = expression(y[i]^T - y[i]^C), adj = c(1.05, 0))
+
+segments(x0 = 0, y0 = 0, x1 = 0, y1 = dnorm(0), lty = "dashed")
+points(0, dnorm(0), pch = 16, col = "red", cex = 1.5)
+
+axis(side = 1, at = 0, labels = expression("Mean causal effect:"~mu[i]^T - mu[i]^"C"))
+text(x = -1.2, y = dnorm(-1.2), label = "Random causal effect", srt = 53, adj = c(.2, -.6))
+dev.off()
