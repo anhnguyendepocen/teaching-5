f2176bc1c46bc19d0f4e3da7df60d70e392ac277
Dag Tanneberg <dag.tanneberg@googlemail.com>
2018-10-08 20:47:29 +0200

    Prep session 5 slides

--- pcqr/r_intro/jonsnow_dies.R
+++ pcqr/r_intro/jonsnow_dies.R
@@ -2,13 +2,47 @@ rm(list = ls())
 books <- c("got", "cok", "sos", "ffc", "dwd")
 # binary, what books did the character appear in?
 length_in_chapters <- c(73, 70, 82, 46, 73)
-
 deaths <- read.csv2(
-  file.path(".", "pcqr", "r_intro", "05", "dta", "asoiaf_deaths.csv"),
+  file.path(".", "pcqr", "r_intro", "05", "dta", "asoiaf.csv"),
   stringsAsFactors = FALSE
 )
 
-table(deaths$allegiances, exclude = NULL)
+book_of_intro_
+for(b in 1:length(books)){
+  book_of_intro <- ifelse(
+    deaths[, "book_of_intro"] == 0 & deaths[, books[b]] == 1, b, deaths[, "book_of_intro"]
+  )
+}
+summary(deaths)
+deaths[, "intro"] <- NA
+filter <- deaths[, "book_of_intro"] == 1
+deaths[filter, ] <- deaths[filter, "book_intro_chapter"]
+
+
+length_in_chapters[1:deaths[1, "book_of_intro"]]
+
+deaths[1, "book_of_intro"]
+# deaths[, "book_intro_chapter"]
+
+
+
+if(deaths[, "book_of_intro"] != 1){
+    sum(c(73, 70, 82, 46, 73)[1:(deaths[, "book_of_intro"] - 1)])
+}
+
+
+deaths <- within(deaths, {
+  entry <- ifelse(
+    book_of_intro != 1,
+    sum(c(73, 70, 82, 46, 73)[1:(book_of_intro - 1)]) + book_intro_chapter,
+    book_intro_chapter
+  )
+  }
+)
+summary(deaths)
+sum(length_in_chapters)
+
+ table(deaths$allegiances, exclude = NULL)
 with(deaths,
   table(death_year, book_of_death, exclude = NULL)
 )
